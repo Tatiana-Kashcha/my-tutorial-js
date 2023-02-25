d3dba56042946030decc7a4e582f28f61b78fe89
Tatiana Kashcha <tkashcha@gmail.com>
2023-02-25 12:09:45 +0200

    Update script.js

--- Lesson_4/script.js
+++ Lesson_4/script.js
@@ -993,12 +993,15 @@
 // addOverNum(20, 74, 11, 62, 46, 12, 36) //повертає 218
 
 
-// Change code below this line
-function findMatches(...args) {
-  const matches = []; // Don't change this line
-
-  // Change code above this line
-  console.log(matches);
-}
-findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) //повертає [1, 2]
-findMatches([63, 11, 8, 29], 4, 7, 16) //повертає []
+// function findMatches(array, ...args) {
+//   const matches = []; // Don't change this line
+//    for (const arr of array) {
+//      if (args.includes(arr)) {
+//        matches.push(arr);
+//      }
+//    }
+  
+//   console.log(matches);
+// }
+// findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) //повертає [1, 2]
+// findMatches([63, 11, 8, 29], 4, 7, 16) //повертає []
