c833376a309ab87d743ddc4002110f23afa33c43
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-03 13:49:27 +0200

    Update script.js

--- Lesson_6-деструкт-дз-мод4/script.js
+++ Lesson_6-деструкт-дз-мод4/script.js
@@ -1663,18 +1663,18 @@
 // ]);
 
 
-// const players = {
-//   mango: 1270,
-//   poly: 468,
-//   ajax: 710,
-//   kiwi: 244,
-// };
-// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244,
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
 
-// const totalPlayTime = playtimes.reduce((previousValue, playTime) => {
-//   return previousValue += playTime;
-// });
-// console.log(totalPlayTime);
-// const averagePlayTime = totalPlayTime / playtimes.length;
+const totalPlayTime = playtimes.reduce((previousValue, playTime) => {
+  return previousValue += playTime;
+});
+console.log(totalPlayTime);
+const averagePlayTime = totalPlayTime / playtimes.length;
 
 
