3c1ac351aa40e8c8f6cf3657f865ea3c84b5dbf7
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-13 20:23:25 +0200

    Update script.js

--- Lesson_10-прототипи-класи/script.js
+++ Lesson_10-прототипи-класи/script.js
@@ -393,29 +393,29 @@
 // і оголошує одну властивість on - стан вкл / викл(true / false).
 // За замовчуванням значення властивості on повинно бути false.
 
-class Toggle {
-  constructor({ isOpen = false } = {}) {
-    this.on = isOpen;
-  }
+// class Toggle {
+//   constructor({ isOpen = false } = {}) {
+//     this.on = isOpen;
+//   }
 
-  toggle() {
-    this.on = !this.on;
-  }
-}
+//   toggle() {
+//     this.on = !this.on;
+//   }
+// }
 
-const firstToggle = new Toggle({ isOpen: true });
-console.group('firstToggle');
-console.log(firstToggle.on);
-firstToggle.toggle();
-console.log(firstToggle.on);
-console.groupEnd('firstToggle');
+// const firstToggle = new Toggle({ isOpen: true });
+// console.group('firstToggle');
+// console.log(firstToggle.on);
+// firstToggle.toggle();
+// console.log(firstToggle.on);
+// console.groupEnd('firstToggle');
 
-const secondToggle = new Toggle();
-console.group('secondToggle');
-console.log(secondToggle.on);
-secondToggle.toggle();
-console.log(secondToggle.on);
-console.groupEnd('secondToggle');
+// const secondToggle = new Toggle();
+// console.group('secondToggle');
+// console.log(secondToggle.on);
+// secondToggle.toggle();
+// console.log(secondToggle.on);
+// console.groupEnd('secondToggle');
 
 //------------------Регістрація юзерів-----------------------
 // const form = document.querySelector('.js-form');
