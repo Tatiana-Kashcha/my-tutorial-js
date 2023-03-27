56cf5163002d35daf74730203e6ff248073de7e3
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-27 23:23:55 +0300

    Update 01-gallery.js

--- Homework-07/js/01-gallery.js
+++ Homework-07/js/01-gallery.js
@@ -1,6 +1,6 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+// Шаблон розмітки галереї
 {
   /* <li class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
@@ -14,4 +14,21 @@ import { galleryItems } from "./gallery-items.js";
 </li>; */
 }
 
+const galleryItemsEl = document.querySelector('.gallery');
+const makeGalleryItems = itemsEl => {
+  const { preview, original, description } = itemsEl;
+  return `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
+};
+const itemsImages = galleryItems.map(makeGalleryItems).join('');
+galleryItemsEl.insertAdjacentHTML('beforeend', itemsImages);
+console.log(galleryItemsEl);
 console.log(galleryItems);
