52ba13d0677dac647f89838beef5c86b84d1df61
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-27 22:30:23 +0300

    додає homework-7

---
+++ Homework-07/js/01-gallery.js
@@ -0,0 +1,17 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+{
+  /* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>; */
+}
+
+console.log(galleryItems);
