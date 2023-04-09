f10adbee7776894c8aa5809dddd77724dada85f8
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-09 20:29:03 +0300

    Update 02-video.js

--- Lesson_15/src/js/02-video.js
+++ Lesson_15/src/js/02-video.js
@@ -16,9 +16,49 @@
 //   return a + b + test;
 // }
 
+// const a = 10;
+// const b = 20;
 
-const a = 10;
-const b = 20;
+// export { a };
+// export default b;
+//--------------------------02 дз---------------------------------
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
-export { a };
-export default b;
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const playerSetItem = function (data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+};
+
+const secondsItem = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const playerGetItem = function (seconds) {
+  secondsItem;
+};
+
+player.on('timeupdate', throttle(playerSetItem, 1000));
+
+player
+  .setCurrentTime(secondsItem)
+  .then(playerGetItem)
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+//------це для ініціалзації плеєра----------------------
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+//------------------------------------------
