4e5500a148de6fa0ad07b777ed1fa883b7e7c7aa
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-24 22:37:57 +0300

    Create lin.js

---
+++ Практика_1-2мод/пр-заняття1-2мод/lin.js
@@ -0,0 +1,6 @@
+import svgLinkdUrl from '../images/linkedin.svg';
+<a href="https://www.linkedin.com/in/nataliia-valko-951501212/" target="_blank" class="team-linkd">
+  <svg width="24" height="24">
+    <use href="${svgLinkdUrl}#icon-linkedin-svg"></use>
+  </svg>
+</a>;
